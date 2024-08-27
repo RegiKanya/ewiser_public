191a86e33c4abff8de3eb6ea6fecbee26027abb2
Regina Kánya <96615693+RegiKanya@users.noreply.github.com>
2024-08-27 16:03:36 +0200

    Update create_connection.py

--- create_connection.py
+++ create_connection.py
@@ -5,16 +5,16 @@ import pandas as pd
 from gsheet_data_filler import filtered_adjusted_inverters,inverters,desired_keys,latest_timestamps
 
 # Create the Client 
-client = pygsheets.authorize(service_account_file="access_details/ewiser_gsheet.json") 
+client = pygsheets.authorize(service_account_file="...") 
 #print(client.spreadsheet_titles()) 
 
 def main():
-    sheet = client.open_by_key('1rv3EPJ8OLlZq2foVWexufG2THvwU_IFXH-GdVlWK938')
+    sheet = client.open_by_key('...')
     data = filtered_adjusted_inverters(inverters, desired_keys, latest_timestamps)
     #check that it's a dataframe if not then convert
     if isinstance(data, list):
         data = pd.DataFrame(data)
-    worksheet = sheet.worksheet_by_title('RAW')  # or worksheet = sheet.worksheet_by_title('Munkalap neve')
+    worksheet = sheet.worksheet_by_title('...')  # or worksheet = sheet.worksheet_by_title('Munkalap neve')
     worksheet.clear()
     worksheet.set_dataframe(data, (1, 1), copy_head=False) #(2nd row, 1st column)
     print("Data has been successfully cleared and reloaded.")
