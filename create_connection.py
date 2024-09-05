7488513eebde6b3d7eba95868f7d336ee82121fb
Regina Kánya <96615693+RegiKanya@users.noreply.github.com>
2024-09-05 15:12:49 +0200

    Update create_connection.py

--- create_connection.py
+++ create_connection.py
@@ -5,7 +5,7 @@ import pandas as pd
 from gsheet_data_filler import filtered_adjusted_inverters,inverters,desired_keys,latest_timestamps, json_file
 
 # Create the Client 
-client = pygsheets.authorize(service_account_file="/Users/kanyaregina/Desktop/access_details/ewiser_gsheet.json") 
+client = pygsheets.authorize(service_account_file="/Users/kanyaregina/Desktop/access_details/gsheet_ewiser.json") 
 #print(client.spreadsheet_titles()) 
 
 def main():
@@ -16,7 +16,7 @@ def main():
         data = pd.DataFrame(data)
     worksheet = sheet.worksheet_by_title('RAW')  # or worksheet = sheet.worksheet_by_title('Munkalap neve')
     worksheet.clear()
-    worksheet.set_dataframe(data, (1, 1), copy_head=False) #(2nd row, 1st column)
+    worksheet.set_dataframe(data, (2, 1), copy_head=False) #(2nd row, 1st column)
     print(f"DONE - Data has been successfully cleared and reloaded {json_file}.")
 
 if __name__ == '__main__':
