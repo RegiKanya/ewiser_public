f92761909539bfa88a1c4a9a21fb51a85dc20c82
Regina Kánya <96615693+RegiKanya@users.noreply.github.com>
2025-01-03 12:27:15 +0100

    formatting the code + changing test code

--- gsheet_data_filler.py
+++ gsheet_data_filler.py
@@ -143,9 +143,9 @@ else:
                 'powerPlantId', 'name', 'locationCity', 'locationParcelNumber', 
                 'totalInverterCount', 'errorInverterCount', 
                 'referenceInverterStatus']
-            pp_id_filter = [408, 409, 415, 455, 1331, 216, 1501, 1502, 1518, 1516, 1513, 1517, 381, 382, 
-                            383, 1476, 146, 145, 144, 1620, 1651, 1649, 1653, 1648, 1546, 1548, 1648, 1655, 
-                            1651, 1653, 1649, 1598, 1621, 1673]
+            pp_id_filter = [144, 145, 146, 216, 381, 382, 383, 408, 409, 415, 455, 1331, 1476, 
+                            1501, 1502, 1513, 1516, 1517, 1518, 1546, 1548, 1598, 1620, 1621, 1648, 
+                            1648, 1649, 1649, 1651, 1651, 1653, 1653, 1655, 1673]
 
             # Assuming latest_timestamps is defined elsewhere in your code
             latest_timestamps = create_timestamp(power_plants)  
--- test/code_testing.py
+++ test/code_testing.py
@@ -1,90 +1,155 @@
 import unittest
 from datetime import datetime
-from gsheet_data_filler import (create_timestamp, create_breakdown_values, filtered_adjusted_inverters, 
-                                format_power_diff_ratio, format_power_diff_quantity)
+from gsheet_data_filler import create_breakdown_values, create_timestamp, filtered_adjusted_inverters
 
 class TestPowerPlantFunctions(unittest.TestCase):
 
     def test_create_timestamp(self):
         power_plants = [
-            {
-                'powerPlantId': 1,
-                'inverters': [
-                    {'startTimestamp': '2023-12-10T15:00:00Z'},
-                    {'startTimestamp': '2023-12-11T10:00:00Z'}
-                ]
-            },
-            {
-                'powerPlantId': 2,
-                'inverters': [
-                    {'startTimestamp': '2023-12-09T12:00:00Z'}
-                ]
-            }
+                     {
+                    "powerPlantId": 1624,
+                    "name": "Eurotrade Gamma Kft - Szeghalom 0800/32",
+                    "locationCity": "Szeghalom",
+                    "locationParcelNumber": "0800/32",
+                    "dataLoggerTypes": [
+                        "HUAWEI"
+                    ],
+                    "usersToNotify": [],
+                    "inverters": [],
+                    "totalInverterCount": 9,
+                    "errorInverterCount": 0,
+                    "unresolvedErrorInverterCount": 0,
+                    "status": "GREEN",
+                    "powerPlantComment": None,
+                    "powerControlGroupId": 2,
+                    "breakDown": None, 
+                    "referenceInverterStatus": "OK",
+                    "inverterPowerDifference": 11.968237294440634,
+                    "inverterPowerDifferenceRatio": 0.025573156612052637,
+                    "inverterPowerEnergyDifference": 6.4208340000000135
+                },
+                {
+                    "powerPlantId": 1178,
+                    "name": "Dendi Sun Kft. Kocsola 030/1",
+                    "locationCity": "Kocsola",
+                    "locationParcelNumber": "033/17",
+                    "dataLoggerTypes": [
+                        "FRONIUS"
+                    ],
+                    "usersToNotify": [],
+                    "inverters": [],
+                    "totalInverterCount": 19,
+                    "errorInverterCount": 0,
+                    "unresolvedErrorInverterCount": 0,
+                    "status": "GREEN",
+                    "powerPlantComment": None,
+                    "powerControlGroupId": 2,
+                    "breakDown": None,
+                    "referenceInverterStatus": "OK",
+                    "inverterPowerDifference": 6.054803376867195,
+                    "inverterPowerDifferenceRatio": 0.01213387450273987,
+                    "inverterPowerEnergyDifference": 3.7331187499998464
+                }
         ]
-
-        expected_output = {
-            1: '2023-12-11',
-            2: '2023-12-09'
+        expected = {
+            1: "2023-01-03",
+            2: "2023-02-01"
         }
-
-        self.assertEqual(create_timestamp(power_plants), expected_output)
+        result = create_timestamp(power_plants)
+        self.assertEqual(result, expected)
 
     def test_create_breakdown_values(self):
-        power_plants = [
-            {
-                'powerPlantId': 1,
-                'breakDown': {'interval': '2023-12-10T00:00:00Z/2023-12-11T00:00:00Z'}
-            },
-            {
-                'powerPlantId': 2,
-                'breakDown': {'interval': '2023-12-09T00:00:00Z/2023-12-09T23:59:59Z'}
-            },
-            {
-                'powerPlantId': 3}
+        data = [
+                        {
+                    "powerPlantId": 1607,
+                    "name": "Helionergy Oberon Kft. - Sarkad 0836/17-23",
+                    "locationCity": "Sarkad",
+                    "locationParcelNumber": "0836/17-23",
+                    "dataLoggerTypes": [],
+                    "usersToNotify": [],
+                    "inverters": [],
+                    "totalInverterCount": 0,
+                    "errorInverterCount": 0,
+                    "unresolvedErrorInverterCount": 0,
+                    "status": "GREEN",
+                    "powerPlantComment": None,
+                    "powerControlGroupId": None,
+                    "breakDown": {
+                        "breakdownEventId": 10243,
+                        "powerPlantId": 1607,
+                        "breakdownPowerWatt": 4172000,
+                        "startDateTime": "2024-01-29T09:00:00.000Z",
+                        "endDateTime": "2025-03-01T07:00:00.000Z",
+                        "creationMode": "Manual",
+                        "editMode": "Manual",
+                        "note": "Termelés megkezdése előtti idő",
+                        "interval": "2024-01-29T09:00:00.000Z/2025-03-01T07:00:00.000Z"
+                    },
+                    "referenceInverterStatus": "OK",
+                    "inverterPowerDifference": 0.0,
+                    "inverterPowerDifferenceRatio": 0.0,
+                    "inverterPowerEnergyDifference": 0.0
+                },
+                {
+                    "powerPlantId": 1660,
+                    "name": "DPV Beta Kft - Döge 0148/11-12",
+                    "locationCity": "Döge",
+                    "locationParcelNumber": "0148/11-12",
+                    "dataLoggerTypes": [],
+                    "usersToNotify": [],
+                    "inverters": [],
+                    "totalInverterCount": 0,
+                    "errorInverterCount": 0,
+                    "unresolvedErrorInverterCount": 0,
+                    "status": "GREEN",
+                    "powerPlantComment": None,
+                    "powerControlGroupId": None,
+                    "breakDown": None,
+                    "referenceInverterStatus": "OK",
+                    "inverterPowerDifference": 0.0,
+                    "inverterPowerDifferenceRatio": 0.0,
+                    "inverterPowerEnergyDifference": 0.0
+                }
         ]
-
-        expected_output = {
-            1: '2023-12-10 -- 2023-12-11',
-            2: '2023-12-09 -- 2023-12-09',
-            3: 'Nincs'
+        expected = {
+            1: "2023-01-01 -- 2023-01-02",
+            2: "2023-02-01 -- 2023-02-03",
+            3: "Nincs"
         }
+        result = create_breakdown_values(data)
+        self.assertEqual(result, expected)
 
-        self.assertEqual(create_breakdown_values({'body': {'modbus': {'powerPlants': power_plants}}}), expected_output)
-
-    def test_format_power_diff_ratio(self):
-        self.assertEqual(format_power_diff_ratio(0.1234), 12.3)
-        self.assertEqual(format_power_diff_ratio(None), None)
-
-    def test_format_power_diff_quantity(self):
-        self.assertEqual(format_power_diff_quantity(12.3456), 12.3)
-        self.assertEqual(format_power_diff_quantity(None), None)
 
     def test_filtered_adjusted_inverters(self):
         inverters = [
             {
-                'powerPlantId': 1,
-                'inverterPowerDifference': 15.6789,
-                'inverterPowerDifferenceRatio': 0.15,
-                'referenceInverterStatus': 'Error'
+                "powerPlantId": 1,
+                "inverterPowerDifferenceRatio": 0.15,
+                "inverterPowerDifference": 100.0,
+                "referenceInverterStatus": "ERROR",
+                "name": "Inverter1",
+                "locationCity": "CityA"
             },
             {
-                'powerPlantId': 2,
-                'inverterPowerDifference': 20.1234,
-                'inverterPowerDifferenceRatio': 0.05,
-                'referenceInverterStatus': 'OK'
+                "powerPlantId": 2,
+                "inverterPowerDifferenceRatio": 0.05,
+                "inverterPowerDifference": 50.0,
+                "referenceInverterStatus": "OK",
+                "name": "Inverter2",
+                "locationCity": "CityB"
             }
         ]
-        desired_keys = ['powerPlantId', 'inverterPowerDifference', 'referenceInverterStatus']
-        latest_timestamps = {1: '2023-12-11'}
-        pp_id_filter = [2]
-        break_down_filtered_date = {1: '2023-12-10 -- 2023-12-11'}
+        desired_keys = ["powerPlantId", "name", "locationCity"]
+        latest_timestamps = {1: "2023-01-03", 2: "2023-02-01"}
+        pp_id_filter = []
+        break_down_filtered_date = {1: "2023-01-01 -- 2023-01-02"}
 
-        expected_output = [
-            [1, None, 'Error', 15.7, 15.0, '2023-12-10 -- 2023-12-11', '2023-12-11']
+        expected = [
+            [1, "Inverter1", "CityA", None, 100.0, 15.0, "2023-01-01 -- 2023-01-02", "2023-01-03"]
         ]
 
         result = filtered_adjusted_inverters(inverters, desired_keys, latest_timestamps, pp_id_filter, break_down_filtered_date)
-        self.assertEqual(result, expected_output)
+        self.assertEqual(result, expected)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
